86cb2da258d77a02fd0bdacda093ebe072baf354
Barnabas Sng <barnabassng10@gmail.com>
2026-01-20 21:01:33 +0800

    Add list function

--- src/main/java/Barn.java
+++ src/main/java/Barn.java
@@ -1,15 +1,24 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Barn {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        ArrayList<String> arr = new ArrayList<String>();
         System.out.println("Hello! I'm Barn\nWhat can I do for you?");
         while (true) {
             String userString = scanner.nextLine();
+            if (userString.equals("list")) {
+                for (int i = 0; i < arr.size(); i++) {
+                    System.out.println(String.valueOf(i+1)+ ". " + arr.get(i));
+                };
+                continue;
+            }
             if (userString.equals("bye")) {
                 break;
             }
-            System.out.println(userString);
+            arr.add(userString);
+            System.out.println("added: "+ userString);
         }
         System.out.println("Bye. Hope to see you again soon!");
     }
