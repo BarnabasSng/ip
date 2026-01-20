6c7001019f9e3e072a9098e79e0f574b25ea17f6
Barnabas Sng <barnabassng10@gmail.com>
2026-01-20 20:35:31 +0800

    Add echo feature

--- src/main/java/Barn.java
+++ src/main/java/Barn.java
@@ -1,6 +1,16 @@
+import java.util.Scanner;
+
 public class Barn {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         System.out.println("Hello! I'm Barn\nWhat can I do for you?");
+        while (true) {
+            String userString = scanner.nextLine();
+            if (userString.equals("bye")) {
+                break;
+            }
+            System.out.println(userString);
+        }
         System.out.println("Bye. Hope to see you again soon!");
     }
 }
