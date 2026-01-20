da9055b7ef40e70da6605fcfd0553ba99f706e81
Barnabas Sng <barnabassng10@gmail.com>
2026-01-20 22:19:06 +0800

    Add ability to mark tasks as done and undone using classes

--- src/main/java/Barn.java
+++ src/main/java/Barn.java
@@ -1,25 +1,38 @@
-import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Barn {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        ArrayList<String> arr = new ArrayList<String>();
+        TaskList tasks = new TaskList();
         System.out.println("Hello! I'm Barn\nWhat can I do for you?");
         while (true) {
             String userString = scanner.nextLine();
+            String firstWord = userString.split(" ")[0];
             if (userString.equals("list")) {
-                for (int i = 0; i < arr.size(); i++) {
-                    System.out.println(String.valueOf(i+1)+ ". " + arr.get(i));
-                };
+                tasks.printList();
                 continue;
             }
             if (userString.equals("bye")) {
                 break;
             }
-            arr.add(userString);
-            System.out.println("added: "+ userString);
+            if (firstWord.equals("mark")) {
+                String[] splitString = userString.split(" ");
+                int index = Integer.parseInt(splitString[1]) - 1;
+                tasks.mark(index);
+                continue;
+            }
+            if (firstWord.equals("unmark")) {
+                String[] splitString = userString.split(" ");
+                int index = Integer.parseInt(splitString[1]) - 1;
+                tasks.unmark(index);
+                continue;
+            }
+            else {
+                Task task = new Task(userString);
+                tasks.add(task);
+            }
         }
         System.out.println("Bye. Hope to see you again soon!");
+        scanner.close();
     }
 }
