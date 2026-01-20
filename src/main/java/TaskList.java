3a0d875e17fa379a391057e6b820c091b5342b81
Barnabas Sng <barnabassng10@gmail.com>
2026-01-20 22:18:07 +0800

    Add TaskList class

---
+++ src/main/java/TaskList.java
@@ -0,0 +1,36 @@
+import java.util.ArrayList;
+
+public class TaskList {
+    protected ArrayList<Task> tasks;
+
+    public TaskList() {
+        this.tasks = new ArrayList<Task>();
+    }
+
+    public void add(Task task) {
+        this.tasks.add(task);
+        System.out.println("added: "+ task);
+    }
+
+    public void printList() {
+        System.out.println("Here are the tasks in your list:");
+        for (int i = 0; i < tasks.size(); i++) {
+            System.out.println(String.valueOf(i+1)+ ".[" 
+                + tasks.get(i).getStatusIcon() + "] " + tasks.get(i));
+        };
+    }
+
+    public void mark(int index) {
+        Task task = tasks.get(index);
+        task.markAsDone();
+        System.out.println("Nice! I've marked this task as done:");
+        System.out.println("[" + task.getStatusIcon() + "] " + task);
+    }
+
+    public void unmark(int index) {
+        Task task = tasks.get(index);
+        task.markAsNotDone();
+        System.out.println("OK, I've marked this task as not done yet:");
+        System.out.println("[" + task.getStatusIcon() + "] " + task);
+    }
+}
