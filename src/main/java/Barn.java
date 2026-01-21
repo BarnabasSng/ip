8ccec32a7f072148d41fdaf1592775a5af5035ef
Barnabas Sng <barnabassng10@gmail.com>
2026-01-21 15:13:44 +0800

    User a custom class to process user input

--- src/main/java/Barn.java
+++ src/main/java/Barn.java
@@ -7,46 +7,45 @@ public class Barn {
         System.out.println("Hello! I'm Barn\nWhat can I do for you?");
         while (true) {
             String userString = scanner.nextLine();
-            String firstWord = userString.split(" ")[0];
-            if (userString.equals("list")) {
+            UserInputProcessor processor = new UserInputProcessor(userString);
+            String command = processor.getCommand();
+            if (command.equals("list")) {
                 tasks.printList();
-                continue;
             }
-            if (userString.equals("bye")) {
+            if (command.equals("bye")) {
                 break;
             }
-            if (firstWord.equals("mark")) {
-                String[] splitString = userString.split(" ");
-                int index = Integer.parseInt(splitString[1]) - 1;
+            if (command.equals("mark")) {
+                int index = processor.getIndex();
                 tasks.mark(index);
-                continue;
             }
-            if (firstWord.equals("unmark")) {
-                String[] splitString = userString.split(" ");
-                int index = Integer.parseInt(splitString[1]) - 1;
+            if (command.equals("unmark")) {
+                int index = processor.getIndex();
                 tasks.unmark(index);
-                continue;
             }
-            if (firstWord.equals("todo")) {
-                String description = userString.split(" ", 2)[1];
+            if (command.equals("todo")) {
+                String description = processor.getTodoInfo();
                 Todo task = new Todo(description);
                 tasks.add(task);
             }
-            if (firstWord.equals("deadline")) {
-                String descriptionAndBy = userString.split(" ", 2)[1];
-                String description = descriptionAndBy.split(" /by ")[0];
-                String by = descriptionAndBy.split(" /by ")[1];
+            if (command.equals("deadline")) {
+                String[] deadlineInfo = processor.getDeadlineInfo();
+                String description = deadlineInfo[0];
+                String by = deadlineInfo[1];
                 Deadline task = new Deadline(description, by);
                 tasks.add(task);
             }
-            if (firstWord.equals("event")) {
-                String descriptionAndFromTo = userString.split(" ", 2)[1];
-                String description = descriptionAndFromTo.split(" /from | /to ")[0];
-                String from = descriptionAndFromTo.split(" /from | /to ")[1];
-                String to = descriptionAndFromTo.split(" /from | /to ")[2];
+            if (command.equals("event")) {
+                String[] eventInfo = processor.getEventInfo();
+                String description = eventInfo[0];
+                String from = eventInfo[1];
+                String to = eventInfo[2];
                 Event task = new Event(description, from, to);
                 tasks.add(task);
             }
+            // else {
+            //     throw new InvalidCommandException();
+            // }
         }
         System.out.println("Bye. Hope to see you again soon!");
         scanner.close();
---
+++ src/main/java/UserInputProcessor.java
@@ -0,0 +1,41 @@
+public class UserInputProcessor {
+    protected String userString;
+
+    public UserInputProcessor(String userString){
+        this.userString = userString;
+    }
+
+    public String getCommand() {
+        String firstWord = this.userString.split(" ")[0];
+        return firstWord;
+    }
+
+    public int getIndex() {
+        String[] splitString = this.userString.split(" ");
+        int index = Integer.parseInt(splitString[1]) - 1;
+        return index;
+    }
+
+    public String getTodoInfo() {
+        String description = this.userString.split(" ", 2)[1];
+        return description;
+    }
+
+    public String[] getDeadlineInfo() {
+        String descriptionAndBy = this.userString.split(" ", 2)[1];
+        String description = descriptionAndBy.split(" /by ")[0];
+        String by = descriptionAndBy.split(" /by ")[1];
+        String[] info = {description, by};
+        return info;
+    }
+
+    public String[] getEventInfo() {
+        String descriptionAndFromTo = this.userString.split(" ", 2)[1];
+        String description = descriptionAndFromTo.split(" /from | /to ")[0];
+        String from = descriptionAndFromTo.split(" /from | /to ")[1];
+        String to = descriptionAndFromTo.split(" /from | /to ")[2];
+        String[] info = {description, from, to};
+        return info;
+    }
+
+}
