8bdd9dbe16d553b3741247e5e3a0ac34db768d44
Barnabas Sng <barnabassng10@gmail.com>
2026-01-27 21:33:32 +0800

    Remove redundant functions and exceptions

--- src/main/java/TaskList.java
+++ src/main/java/TaskList.java
@@ -11,46 +11,33 @@ public class TaskList {
         this.tasks = tasks;
     }
 
-    private void printTaskSize() {
-        System.out.println("Now you have " + tasks.size() + " tasks in the list.");
+    public int getTaskCount() {
+        return tasks.size();
+    }
+
+    public Task getTask(int index) {
+        return tasks.get(index);
     }
 
     public void add(Task task){
-        this.tasks.add(task);
-        System.out.println("Got it, I've added this task: \n"+ task);
-        printTaskSize();
-    }
-
-    public void printList() {
-        System.out.println("Here are the tasks in your list:");
-        for (int i = 0; i < tasks.size(); i++) {
-            System.out.println(String.valueOf(i+1)+ "." + tasks.get(i));
-        };
+        tasks.add(task);
     }
 
     public void mark(int index) {
         Task task = tasks.get(index);
         task.markAsDone();
-        System.out.println("Nice! I've marked this task as done:");
-        System.out.println(task);
     }
 
     public void unmark(int index) {
         Task task = tasks.get(index);
         task.markAsNotDone();
-        System.out.println("OK, I've marked this task as not done yet:");
-        System.out.println(task);
     }
 
     public void delete(int index) {
-        Task task = tasks.get(index);
         tasks.remove(index);
-        System.out.println("Noted. I've removed this task:");
-        System.out.println(task);
-        printTaskSize();
     }
 
     public ArrayList<Task> getArr() {
-        return this.tasks;
+        return tasks;
     }
 }
