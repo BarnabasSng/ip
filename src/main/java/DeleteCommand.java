0707552cc6c0b7e134976ea47a4e65d20ec4c6b7
Barnabas Sng <barnabassng10@gmail.com>
2026-01-27 21:29:53 +0800

    Add Command classes

---
+++ src/main/java/DeleteCommand.java
@@ -0,0 +1,24 @@
+public class DeleteCommand extends Command{
+
+    protected int index;
+
+    public DeleteCommand(int index) {
+        this.index = index;
+    }
+
+    public void execute(TaskList tasks, Ui ui, Storage storage) {
+        try {
+            Task task = tasks.getTask(index);
+            tasks.delete(index);
+            ui.showDeleteTask(task);
+            ui.showTaskCount(tasks);
+            storage.save(tasks);
+        } catch (Exception e) {
+            ui.showError(e.getMessage());;
+        }
+    }
+
+    public boolean isExit() {
+        return false;
+    }
+}
---
+++ src/main/java/UnmarkCommand.java
@@ -0,0 +1,23 @@
+public class UnmarkCommand extends Command{
+
+    protected int index;
+
+    public UnmarkCommand(int index) {
+        this.index = index;
+    }
+
+    public void execute(TaskList tasks, Ui ui, Storage storage) {
+        try {
+            Task task = tasks.getTask(index);
+            tasks.unmark(index);
+            ui.showUnmark(task);
+            storage.save(tasks);
+        } catch (Exception e) {
+            ui.showError(e.getMessage());
+        }
+    }
+
+    public boolean isExit() {
+        return false;
+    }
+}
