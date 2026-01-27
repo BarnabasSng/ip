9631233523d19c0e135b147239e7711bd96fe7cc
Barnabas Sng <barnabassng10@gmail.com>
2026-01-27 15:19:58 +0800

    Add Ui class

---
+++ src/main/java/Ui.java
@@ -0,0 +1,17 @@
+public class Ui {
+    public void showWelcome() {
+        System.out.println("Hello! I'm Barn\nWhat can I do for you?");
+    }
+
+    public  void showLine() {
+        System.out.println("____________________________________________________________");
+    }
+
+    public void showError(String error) {
+        System.out.println(error);
+    }
+
+    public void showExit() {
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+}
