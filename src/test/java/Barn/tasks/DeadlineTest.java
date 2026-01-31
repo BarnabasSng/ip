141a7caac3ebc4c7d02c057da5ff29557a70cc7e
Barnabas Sng <barnabassng10@gmail.com>
2026-01-31 22:06:04 +0800

    Add JUnit testing for Deadline getDate method

---
+++ src/test/java/Barn/tasks/DeadlineTest.java
@@ -0,0 +1,20 @@
+package Barn.tasks;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class DeadlineTest {
+    @Test
+    public void getDate_validDateFormat_success(){
+        String date = "2026-02-10";
+        String formattedDate = "Feb 10 2026";
+        assertEquals(formattedDate, new Deadline("test", date).getDate());
+    }
+
+    @Test
+    public void getDate_invalidDateFormat_success() {
+        String date = "tomorrow";
+        assertEquals(date, new Deadline("test", date).getDate());
+    }
+}
