5540a1ba8cfc2c43b30e4d2e746c851d997a77ed
Barnabas Sng <barnabassng10@gmail.com>
2026-02-02 00:17:14 +0800

    Change Parser class to use regex patterns

--- src/main/java/Barn/parser/Parser.java
+++ src/main/java/Barn/parser/Parser.java
@@ -1,14 +1,16 @@
 package Barn.parser;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import Barn.commands.AddCommand;
 import Barn.commands.Command;
 import Barn.commands.DeleteCommand;
 import Barn.commands.ExitCommand;
 import Barn.commands.FindCommand;
+import Barn.commands.ListCommand;
 import Barn.commands.MarkCommand;
-import Barn.commands.ShowTasksCommand;
 import Barn.commands.UnmarkCommand;
-import Barn.exceptions.EmptyDescriptionException;
 import Barn.exceptions.InvalidCommandException;
 import Barn.tasks.Deadline;
 import Barn.tasks.Event;
@@ -20,114 +22,167 @@ import Barn.tasks.Todo;
  */
 public class Parser {
 
-    private static String getFirstWord(String fullCommand) {
-        String firstWord = fullCommand.split(" ")[0];
-        return firstWord;
+    public enum CommandType {
+        TODO,
+        DEADLINE,
+        EVENT,
+        MARK,
+        UNMARK,
+        DELETE,
+        FIND,
+        LIST,
+        BYE
     }
 
-    private static int getIndex(String fullCommand) {
-        String[] splitString = fullCommand.split(" ");
-        int index = Integer.parseInt(splitString[1]) - 1;
-        return index;
-    }
+    public static final Pattern BASIC_COMMAND_FORMAT = Pattern.compile("(?<commandWord>\\S+)(?<arguments>.*)");
 
-    private static String getTodoInfo(String fullCommand) throws EmptyDescriptionException {
-        if (fullCommand.split(" ").length <= 1) {
-            throw new EmptyDescriptionException();
-        }
-        String description = fullCommand.split(" ", 2)[1];
-        return description;
-    }
+    public static final Pattern TODO_ARGS_FORMAT = Pattern.compile("(?<description>.+)");
 
-    private static String[] getDeadlineInfo(String fullCommand) {
-        String descriptionAndBy = fullCommand.split(" ", 2)[1];
-        String description = descriptionAndBy.split(" /by ")[0];
-        String by = descriptionAndBy.split(" /by ")[1];
-        String[] info = { description, by };
-        return info;
-    }
+    public static final Pattern DEADLINE_ARGS_FORMAT = Pattern.compile("(?<description>[^/]+)"
+            + " /by (?<by>.+)");
 
-    private static String[] getEventInfo(String fullCommand) {
-        String descriptionAndFromTo = fullCommand.split(" ", 2)[1];
-        String description = descriptionAndFromTo.split(" /from | /to ")[0];
-        String from = descriptionAndFromTo.split(" /from | /to ")[1];
-        String to = descriptionAndFromTo.split(" /from | /to ")[2];
-        String[] info = { description, from, to };
-        return info;
-    }
+    public static final Pattern EVENT_ARGS_FORMAT = Pattern.compile("(?<description>[^/]+)"
+            + " /from (?<from>[^/]+)"
+            + " /to (?<to>.+)");
 
-    private static String getKeyword(String fullCommand) {
-        String keyword = fullCommand.split(" ")[1];
-        return keyword;
-    }
+    public static final Pattern MARK_UNMARK_DELETE_ARGS_FORMAT = Pattern.compile("\\s*(?<index>\\d+)\\s*");
+
+    public static final Pattern FIND_ARGS_FORMAT = Pattern.compile("(?<keyword>.+)");
 
     /**
      * Processes user command and returns a Command class object.
      * 
      * @param fullCommand Full string given by the user
      * @return Command class object representing the user command
-     * @throws InvalidCommandException If fullCommand is of an invalid format
+     * @throws InvalidCommandException If userInput is of an invalid format
      */
-    public static Command parse(String fullCommand) throws InvalidCommandException {
-        try {
-            String firstWord = getFirstWord(fullCommand);
-            switch (firstWord) {
-                case "todo": {
-                    String description = getTodoInfo(fullCommand);
-                    Todo task = new Todo(description);
-                    return new AddCommand(task);
-                }
-
-                case "deadline": {
-                    String[] deadlineInfo = getDeadlineInfo(fullCommand);
-                    String description = deadlineInfo[0];
-                    String by = deadlineInfo[1];
-                    Deadline task = new Deadline(description, by);
-                    return new AddCommand(task);
-                }
-
-                case "event": {
-                    String[] eventInfo = getEventInfo(fullCommand);
-                    String description = eventInfo[0];
-                    String from = eventInfo[1];
-                    String to = eventInfo[2];
-                    Event task = new Event(description, from, to);
-                    return new AddCommand(task);
-                }
-
-                case "mark": {
-                    int index = getIndex(fullCommand);
-                    return new MarkCommand(index);
-                }
-
-                case "unmark": {
-                    int index = getIndex(fullCommand);
-                    return new UnmarkCommand(index);
-                }
-
-                case "delete": {
-                    int index = getIndex(fullCommand);
-                    return new DeleteCommand(index);
-                }
-
-                case "list": {
-                    return new ShowTasksCommand();
-                }
-
-                case "bye": {
-                    return new ExitCommand();
-                }
-
-                case "find": {
-                    String keyword = getKeyword(fullCommand);
-                    return new FindCommand(keyword);
-                }
-
-                default:
-                    throw new InvalidCommandException();
-            }
-        } catch (Exception e) {
+    public static Command parse(String userInput) throws InvalidCommandException {
+        Matcher matcher = BASIC_COMMAND_FORMAT.matcher(userInput.trim());
+        if (!matcher.matches()) {
             throw new InvalidCommandException();
         }
+
+        String commandWord = matcher.group("commandWord").toUpperCase();
+        String arguments = matcher.group("arguments");
+
+        CommandType commandType;
+        try {
+            commandType = CommandType.valueOf(commandWord);
+        } catch (IllegalArgumentException e) {
+            throw new InvalidCommandException();
+        }
+
+        switch (commandType) {
+            case TODO:
+                return parseTodo(arguments);
+
+            case EVENT:
+                return parseEvent(arguments);
+
+            case DEADLINE:
+                return parseDeadline(arguments);
+
+            case MARK:
+                return parseMark(arguments);
+
+            case UNMARK:
+                return parseUnmark(arguments);
+
+            case DELETE:
+                return parseDelete(arguments);
+
+            case FIND:
+                return parseFind(arguments);
+
+            case LIST:
+                return new ListCommand();
+
+            case BYE:
+                return new ExitCommand();
+
+            default:
+                throw new InvalidCommandException();
+        }
+    }
+
+    private static Command parseTodo(String arguments) throws InvalidCommandException {
+        Matcher matcher = TODO_ARGS_FORMAT.matcher(arguments.trim());
+        if (!matcher.matches()) {
+            throw new InvalidCommandException();
+        }
+        String description = matcher.group("description");
+        return new AddCommand(new Todo(description));
+    }
+
+    private static Command parseDeadline(String arguments) throws InvalidCommandException {
+        Matcher matcher = DEADLINE_ARGS_FORMAT.matcher(arguments.trim());
+        if (!matcher.matches()) {
+            throw new InvalidCommandException();
+        }
+        String description = matcher.group("description");
+        String by = matcher.group("by");
+        return new AddCommand(new Deadline(description, by));
+    }
+
+    private static Command parseEvent(String arguments) throws InvalidCommandException {
+        Matcher matcher = EVENT_ARGS_FORMAT.matcher(arguments.trim());
+        if (!matcher.matches()) {
+            throw new InvalidCommandException();
+        }
+        String description = matcher.group("description");
+        String from = matcher.group("from");
+        String to = matcher.group("to");
+        return new AddCommand(new Event(description, from, to));
+    }
+
+    private static Command parseMark(String arguments) throws InvalidCommandException {
+        Matcher matcher = MARK_UNMARK_DELETE_ARGS_FORMAT.matcher(arguments.trim());
+        if (!matcher.matches()) {
+            throw new InvalidCommandException();
+        }
+        int index;
+        try {
+            index = Integer.parseInt(matcher.group("index")) - 1;
+        } catch (NumberFormatException e) {
+            throw new InvalidCommandException();
+        }
+        return new MarkCommand(index);
+    }
+
+    private static Command parseUnmark(String arguments) throws InvalidCommandException {
+        Matcher matcher = MARK_UNMARK_DELETE_ARGS_FORMAT.matcher(arguments.trim());
+        if (!matcher.matches()) {
+            throw new InvalidCommandException();
+        }
+        int index;
+        try {
+            index = Integer.parseInt(matcher.group("index")) - 1;
+        } catch (NumberFormatException e) {
+            throw new InvalidCommandException();
+        }
+        return new UnmarkCommand(index);
+    }
+
+    private static Command parseDelete(String arguments) throws InvalidCommandException {
+        Matcher matcher = MARK_UNMARK_DELETE_ARGS_FORMAT.matcher(arguments.trim());
+        if (!matcher.matches()) {
+            throw new InvalidCommandException();
+        }
+        int index;
+        try {
+            index = Integer.parseInt(matcher.group("index")) - 1;
+        } catch (NumberFormatException e) {
+            throw new InvalidCommandException();
+        }
+        return new DeleteCommand(index);
+    }
+
+    private static Command parseFind(String arguments) throws InvalidCommandException {
+        Matcher matcher = FIND_ARGS_FORMAT.matcher(arguments.trim());
+        if (!matcher.matches()) {
+            throw new InvalidCommandException();
+        }
+        String keyword = matcher.group("keyword");
+        return new FindCommand(keyword);
     }
 }
