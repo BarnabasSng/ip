ea0cc9bb8b65f07f55cd2191fe22f3c744b48d1b
Barnabas Sng <barnabassng10@gmail.com>
2026-02-02 01:18:26 +0800

    Change Storage class to use regex pattern

--- src/main/java/Barn/storage/Storage.java
+++ src/main/java/Barn/storage/Storage.java
@@ -5,6 +5,8 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import Barn.exceptions.LoadingException;
 import Barn.tasks.Deadline;
@@ -17,6 +19,23 @@ import Barn.tasks.tasklist.TaskList;
  * Manages loading and writing tasks into the txt file in hard drive.
  */
 public class Storage {
+
+    public enum TaskType {
+        T,
+        D,
+        E
+    }
+
+    public static final Pattern TASK_SAVE_FORMAT = Pattern.compile("^(?<taskType>[TDE]) \\| (?<arguments>.*)$");
+
+    public static final Pattern TODO_SAVE_FORMAT = Pattern.compile("(?<doneFlag>[01]) \\| (?<description>.+)");
+
+    public static final Pattern DEADLINE_SAVE_FORMAT = Pattern
+            .compile("(?<doneFlag>[01]) \\| (?<description>.+) \\| (?<by>.+)");
+
+    public static final Pattern EVENT_SAVE_FORMAT = Pattern
+            .compile("(?<doneFlag>[01]) \\| (?<description>.+) \\| (?<from>.+) \\| (?<to>.+)");
+
     protected String filePath;
 
     public Storage(String filePath) {
@@ -38,36 +57,40 @@ public class Storage {
         if (!f.exists()) {
             f.createNewFile();
         }
+
         Scanner s = new Scanner(f);
         while (s.hasNextLine()) {
-            try {
-                String[] splitLine = s.nextLine().split("\\s*\\|\\s*");
-                String taskType = splitLine[0];
-                int done = Integer.parseInt(splitLine[1]);
-                String description = splitLine[2];
-                Task task;
-                switch (taskType) {
-                    case "T":
-                        task = new Todo(description, done);
-                        break;
-
-                    case "D":
-                        String by = splitLine[3];
-                        task = new Deadline(description, done, by);
-                        break;
-
-                    case "E":
-                        String from = splitLine[3];
-                        String to = splitLine[4];
-                        task = new Event(description, done, from, to);
-                        break;
-                    default:
-                        throw new LoadingException();
-                }
-                tasks.add(task);
-            } catch (Exception e) {
+            String line = s.nextLine();
+            Matcher matcher = TASK_SAVE_FORMAT.matcher(line.trim());
+            if (!matcher.matches()) {
                 throw new LoadingException();
             }
+
+            String taskLetter = matcher.group("taskType").toUpperCase();
+            String arguments = matcher.group("arguments");
+
+            TaskType taskType;
+            try {
+                taskType = TaskType.valueOf(taskLetter);
+            } catch (IllegalArgumentException e) {
+                throw new LoadingException();
+            }
+
+            Task task;
+            switch (taskType) {
+                case T:
+                    task = getTodoTask(arguments);
+                    break;
+                case D:
+                    task = getDeadlineTask(arguments);
+                    break;
+                case E:
+                    task = getEventTask(arguments);
+                    break;
+                default:
+                    throw new LoadingException();
+            }
+            tasks.add(task);
         }
         s.close();
         return tasks;
@@ -85,5 +108,47 @@ public class Storage {
             fw.write(task.getFormattedString() + "\n");
         }
         fw.close();
+    }
+
+    private Task getTodoTask(String arguments) throws LoadingException {
+        Matcher matcher = TODO_SAVE_FORMAT.matcher(arguments);
+        if (!matcher.matches()) {
+            throw new LoadingException();
+        }
+        String description = matcher.group("description");
+        Task task = new Todo(description);
+        if (matcher.group("doneFlag").equals("1")) {
+            task.markAsDone();
+        }
+        return task;
+    }
+
+    private Task getDeadlineTask(String arguments) throws LoadingException {
+        Matcher matcher = DEADLINE_SAVE_FORMAT.matcher(arguments);
+        if (!matcher.matches()) {
+            throw new LoadingException();
+        }
+        String description = matcher.group("description");
+        String by = matcher.group("by");
+        Task task = new Deadline(description, by);
+        if (matcher.group("doneFlag").equals("1")) {
+            task.markAsDone();
+        }
+        return task;
+    }
+
+    private Task getEventTask(String arguments) throws LoadingException {
+        Matcher matcher = EVENT_SAVE_FORMAT.matcher(arguments);
+        if (!matcher.matches()) {
+            throw new LoadingException();
+        }
+        String description = matcher.group("description");
+        String from = matcher.group("from");
+        String to = matcher.group("to");
+        Task task = new Event(description, from, to);
+        if (matcher.group("doneFlag").equals("1")) {
+            task.markAsDone();
+        }
+        return task;
     }
 }
