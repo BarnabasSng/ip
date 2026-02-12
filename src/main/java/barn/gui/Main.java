d7ac022aaf0ccfa2441a28d6c75aec17a416fb08
Barnabas Sng <barnabassng10@gmail.com>
2026-02-12 12:05:57 +0800

    Set dimensions of app

--- src/main/java/barn/gui/Main.java
+++ src/main/java/barn/gui/Main.java
@@ -23,6 +23,8 @@ public class Main extends Application {
             AnchorPane ap = fxmlLoader.load();
             Scene scene = new Scene(ap);
             stage.setScene(scene);
+            stage.setMinHeight(220);
+            stage.setMinWidth(417);
             fxmlLoader.<MainWindow>getController().setBarn(barn); // inject the Barn instance
             stage.show();
         } catch (IOException e) {
